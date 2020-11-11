12cbe508d29c5a1075b363fbbd76053b62ccc36a
Shengyou Fan <shengyoufan@gmail.com>
2020-11-11 15:18:07 +0800

    add kotlinx.datetime dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     jcenter()
     mavenCentral()
+    maven(url = "https://kotlin.bintray.com/kotlinx/")
 }
 
 dependencies {
     implementation("com.github.javafaker:javafaker:1.0.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
     testImplementation(kotlin("test-junit"))
 }
 
