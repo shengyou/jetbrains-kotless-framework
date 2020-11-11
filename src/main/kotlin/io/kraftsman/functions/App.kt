cdded9ea4be33cb4f0e92f69cfbb0bf57d02d353
Shengyou Fan <shengyoufan@gmail.com>
2020-11-11 15:35:25 +0800

    implement function handler

--- src/main/kotlin/io/kraftsman/functions/App.kt
+++ src/main/kotlin/io/kraftsman/functions/App.kt
@@ -1,8 +1,25 @@
 package io.kraftsman.functions
 
+import com.github.javafaker.Faker
+import io.kotless.dsl.lang.KotlessContext
 import io.kotless.dsl.lang.http.Get
 import io.kotless.dsl.lang.http.html
+import io.kotless.dsl.lang.http.json
 import io.kotless.dsl.model.HttpResponse
+import io.kraftsman.dtos.Contact
+import io.kraftsman.dtos.ContactApiResponse
+import io.kraftsman.services.ContactService
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.serialization.builtins.list
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import java.util.*
+import kotlin.random.Random
+import kotlin.time.ExperimentalTime
+import kotlin.time.hours
+import kotlin.time.minutes
 
 @Get("/")
 fun home(): HttpResponse {
@@ -20,4 +37,19 @@ fun home(): HttpResponse {
 </html>""".trimIndent()
 
     return html(dom)
+}
+
+@ExperimentalTime
+@Get("/contacts")
+fun contactAPI(): HttpResponse {
+
+    val param = KotlessContext.HTTP.request.params?.get("amount")
+    val amount = param?.toIntOrNull() ?: 10
+
+    val contacts = ContactService().generate(amount)
+    val contactResponse = ContactApiResponse(contacts)
+    val json = Json(JsonConfiguration.Stable)
+    val jsonString = json.stringify(ContactApiResponse.serializer(), contactResponse)
+
+    return json(jsonString)
 }
