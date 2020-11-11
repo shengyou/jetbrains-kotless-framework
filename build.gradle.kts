6484933650cf4076d40e5525c23d9ec96ef34501
Shengyou Fan <shengyoufan@gmail.com>
2020-11-11 15:14:53 +0800

    downgrade Kotlin to 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.3.72" apply true
 }
+
 group = "io.kraftsman"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
+
 dependencies {
     testImplementation(kotlin("test-junit"))
 }
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "11"
 }
