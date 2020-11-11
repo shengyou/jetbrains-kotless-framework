85489fb32ce91bacf385adcffaf18e0de67f0fb7
Shengyou Fan <shengyoufan@gmail.com>
2020-11-11 15:19:21 +0800

    add kotless dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import io.kotless.plugin.gradle.dsl.Webapp.Route53
+import io.kotless.plugin.gradle.dsl.kotless
 
 plugins {
     kotlin("jvm") version "1.3.72" apply true
     kotlin("plugin.serialization") version "1.3.72"
+    id("io.kotless") version "0.1.6" apply true
 }
 
 group = "io.kraftsman"
@@ -17,6 +20,7 @@ repositories {
 dependencies {
     implementation("com.github.javafaker:javafaker:1.0.2")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.0")
+    implementation("io.kotless", "kotless-lang", "0.1.6")
     testImplementation(kotlin("test-junit"))
 }
 
